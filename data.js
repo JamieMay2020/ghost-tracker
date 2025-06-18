36fa051541e86181dd597f1ab681e3d878fb448d
Jamie May <109759539+JamieMay2020@users.noreply.github.com>
2025-06-19 00:32:23 +0100

    Update data.js

--- data.js
+++ data.js
@@ -119,109 +119,109 @@ const GHOST_DATA = {
             address: "8yJFWmVTQq69p6VJxGwpzW7ii7c5J9GRAtHCNMMQPydj",
             balance: 20.258104897
         },
-
     ],
 
-    
-    // TRANSFERS - Add transfer alerts here (newest first)
-transfers: [
-    {
-        walletName: "pow",
-        from: "8zFZHuSRuDpuAR7J6FzwyF3vKNx4CVW3DFHJerQhc7Zd",
-        to: "GRtUWR9VuXgTxaV3GL5cgdqEs799EhQzxpg6RUUSLLUK",
-        amount: 70,
-        signature: "fvKSb8upwLTkhLvvmnSrXG5tHoDiC72BqyeXzLz3z4EL5V3qAjBtg4DEevpovYB27oW1JkCvZUewsEnvwQrGaCW",
-        timestamp: "June 16, 2025 18:04:12 +UTC"
-    },
-    {
-        walletName: "cupsey",
-        from: "suqh5sHtr8HyJ7q8scBimULPkPpA557prMG47xCHQfK",
-        to: "CTyFguG69kwYrzk24P3UuBvY1rR5atu9kf2S6XEwAU8X",
-        amount: 1000,
-        signature: "5tfWAiUqo4nuzENT9uhsakwbWR8pNYE8MJmiLTphhgVizHuDWqmq44aDg5WonbSeH92E33hzZjMVzL6AJRcC5evh",
-        timestamp: "June 16, 2025 16:39:50 +UTC"
-    },
-    {
-        walletName: "pow",
-        from: "8zFZHuSRuDpuAR7J6FzwyF3vKNx4CVW3DFHJerQhc7Zd",
-        to: "zWEbocB2viSwiADFEQ9QfL6rYwQFVQ91fPkSRhTd7TW",
-        amount: 650.0,
-        signature: "HC3MPUKHefFrqQM4ftxeLiXL9QfTHPBxHZ5ot6o13tiey21YYrYiCEtakuHTys8MyyCeF1sd3eSQaFN5uMjEBeN",
-        timestamp: "June 07, 2025 17:34:38 +UTC"
-    },
-    {
-        walletName: "cupsey",
-        from: "suqh5sHtr8HyJ7q8scBimULPkPpA557prMG47xCHQfK",
-        to: "CTyFguG69kwYrzk24P3UuBvY1rR5atu9kf2S6XEwAU8X",
-        amount: 1000,
-        signature: "2V95MgNMZiME2UEGa3Sb3vHozyVnqXV71KBzMELKBa4nRM1Sy7Nf9MYvuQcd26MjBzdV6u812iReXXz6PahcxFJp",
-        timestamp: "June 11, 2025 03:26:09 +UTC"
-    },
-    {
-        walletName: "cented",
-        from: "CyaE1VxvBrahnPWkqm5VsdCvyS2QmNht2UFrKJHga54o",
-        to: "2Zk4Dz3qyrKodv3yYdCTEidfJHcyM4fpj7huUGoLAwU7",
-        amount: 150,
-        signature: "2m8A45KHcpwnz5Mbv4fjMuA56J8r2YPCUqZaqRSSsXf58ExoZQnQucsTZ8ssV6bAmg6fmBAMZzTfjvE4vU2WrBde",
-        timestamp: "June 13, 2025 00:12:00 +UTC"
-    },
-    {
-        walletName: "kreo",
-        from: "BCnqsPEtA1TkgednYEebRpkmwFRJDCjMQcKZMMtEdArc",
-        to: "BJqrMMVFrMTM9V93w2JN2uv73WX9JxZgEbbFdevWEt6x",
-        amount: 218.0,
-        signature: "5foDH89ARvbZ4QyS639eEkxxzpDLd5e6F5shxjesxMm9PpdJL5D6GqFMMaGNYBx4Pe1dhx7orNGo6WJ8Mjv1V2bw",
-        timestamp: "June 06, 2025 23:29:11 +UTC"
-    },
-    {
-        walletName: "pow",
-        from: "GRtUWR9VuXgTxaV3GL5cgdqEs799EhQzxpg6RUUSLLUK",
-        to: "8zFZHuSRuDpuAR7J6FzwyF3vKNx4CVW3DFHJerQhc7Zd",
-        amount: 671.497,
-        signature: "2ZnpiL7pJsdqnkP7vh4N2uidnj3Zt7jp14MA8nnzuLQcdD8jznSDTrDd8kc2VbuvDjidS2RRFLJpzkDrfk7S1nf9",
-        timestamp: "June 06, 2025 23:03:59 +UTC"
-    },
-    {
-        walletName: "gorilla",
-        from: "HK2TKdCZM8j3dAhR4wUrsa28MVKn6vgLPMi7QhsBHgXM",
-        to: "DpNVrtA3ERfKzX4F8Pi2CVykdJJjoNxyY5QgoytAwD26",
-        amount: 142.68,
-        signature: "4Z1PRwdX4NSa9BdGsr2z9Xf2FNcYhGyNHXC5U99UDuHBhnJ69siXUxiavHjnZV6GAq5dm5vXdJRWh96EThyenhvC",
-        timestamp: "June 06, 2025 22:08:59 +UTC"
-    },
-    {
-        walletName: "lexapro",
-        from: "9YYGgphrSopDBaQTAxpuqwZDLgdRjJWhYQCmAFrfqrXq",
-        to: "8yJFWmVTQq69p6VJxGwpzW7ii7c5J9GRAtHCNMMQPydj",
-        amount: 100.0,
-        signature: "5JJw3k4kb6PUjVc5tGEs7ygPbPYK7k2STU69cMEkroRNZBK48z4xots4TXht6FZDbckZEmsprqxpc5Fxxp9gnPvp",
-        timestamp: "June 05, 2025 05:21:28 +UTC"
-    },
-    {
-        walletName: "meechie",
-        from: "6qkh2JcHt3ctFeiL4HBn1e9NU5aPw25XNhtgEv6ZCJ4U",
-        to: "831qmkeGhfL8YpcXuhrug6nHj1YdK3aXMDQUCo85Auh1",
-        amount: 101.991,
-        signature: "4U9sKJYpLfBNz93KvJP5yAEPtGsHAPp37rgGjTbUEqKWKnBBG8Lbxi2daQiT8q7WJXxyyVLtP6AM5quvA5gTyNSk",
-        timestamp: "May 13, 2025 13:54:09 +UTC"
-    },
-    {
-        walletName: "kaden",
-        from: "3pZ59YENxDAcjaKa3sahZJBcgER4rGYi4v6BpPurmsGj",
-        to: "BNismNsqqCKLcMzJeM2x2nMx53VMKytvkNKubJ1Tx9QC",
-        amount: 161.13,
-        signature: "2TUw5XKHX3YoK5tsJDe5j8LK7U2Fu2eZB1yiWxuHDrBfxexF5m2DFR76RnnX3JKonNWTndszgNboEtTeuQ93ahdu",
-        timestamp: "May 05, 2025 05:23:07 +UTC"
-    },
-    {
-        walletName: "Jalen",
-        from: "F72vY99ihQsYwqEDCfz7igKXA5me6vN2zqVsVUTpw6qL",
-        to: "CW9qWGDXQqDAKeRyPbBZ352cKgNqEUtpfKrR7n9PqZa5",
-        amount: 494.71,
-        signature: "5cDBEC5mKctZ7JpqRjbgtZDG2JBqND1SAFUx5gzh4dqtEL9gth1CaBqqb3FHjG5EGv6KoxCxztvZWp32x3vehJqF",
-        timestamp: "March 05, 2025 20:55:51 +UTC"
-    }
-]
+    // TRANSFERS - Ordered from most recent to oldest
+    transfers: [
+        {
+            walletName: "pow",
+            from: "8zFZHuSRuDpuAR7J6FzwyF3vKNx4CVW3DFHJerQhc7Zd",
+            to: "GRtUWR9VuXgTxaV3GL5cgdqEs799EhQzxpg6RUUSLLUK",
+            amount: 70,
+            signature: "fvKSb8upwLTkhLvvmnSrXG5tHoDiC72BqyeXzLz3z4EL5V3qAjBtg4DEevpovYB27oW1JkCvZUewsEnvwQrGaCW",
+            timestamp: "June 16, 2025 18:04:12 +UTC"
+        },
+        {
+            walletName: "cupsey",
+            from: "suqh5sHtr8HyJ7q8scBimULPkPpA557prMG47xCHQfK",
+            to: "CTyFguG69kwYrzk24P3UuBvY1rR5atu9kf2S6XEwAU8X",
+            amount: 1000,
+            signature: "5tfWAiUqo4nuzENT9uhsakwbWR8pNYE8MJmiLTphhgVizHuDWqmq44aDg5WonbSeH92E33hzZjMVzL6AJRcC5evh",
+            timestamp: "June 16, 2025 16:39:50 +UTC"
+        },
+        {
+            walletName: "cented",
+            from: "CyaE1VxvBrahnPWkqm5VsdCvyS2QmNht2UFrKJHga54o",
+            to: "2Zk4Dz3qyrKodv3yYdCTEidfJHcyM4fpj7huUGoLAwU7",
+            amount: 150,
+            signature: "2m8A45KHcpwnz5Mbv4fjMuA56J8r2YPCUqZaqRSSsXf58ExoZQnQucsTZ8ssV6bAmg6fmBAMZzTfjvE4vU2WrBde",
+            timestamp: "June 13, 2025 00:12:00 +UTC"
+        },
+        {
+            walletName: "cupsey",
+            from: "suqh5sHtr8HyJ7q8scBimULPkPpA557prMG47xCHQfK",
+            to: "CTyFguG69kwYrzk24P3UuBvY1rR5atu9kf2S6XEwAU8X",
+            amount: 1000,
+            signature: "2V95MgNMZiME2UEGa3Sb3vHozyVnqXV71KBzMELKBa4nRM1Sy7Nf9MYvuQcd26MjBzdV6u812iReXXz6PahcxFJp",
+            timestamp: "June 11, 2025 03:26:09 +UTC"
+        },
+        {
+            walletName: "pow",
+            from: "8zFZHuSRuDpuAR7J6FzwyF3vKNx4CVW3DFHJerQhc7Zd",
+            to: "zWEbocB2viSwiADFEQ9QfL6rYwQFVQ91fPkSRhTd7TW",
+            amount: 650.0,
+            signature: "HC3MPUKHefFrqQM4ftxeLiXL9QfTHPBxHZ5ot6o13tiey21YYrYiCEtakuHTys8MyyCeF1sd3eSQaFN5uMjEBeN",
+            timestamp: "June 07, 2025 17:34:38 +UTC"
+        },
+        {
+            walletName: "kreo",
+            from: "BCnqsPEtA1TkgednYEebRpkmwFRJDCjMQcKZMMtEdArc",
+            to: "BJqrMMVFrMTM9V93w2JN2uv73WX9JxZgEbbFdevWEt6x",
+            amount: 218.0,
+            signature: "5foDH89ARvbZ4QyS639eEkxxzpDLd5e6F5shxjesxMm9PpdJL5D6GqFMMaGNYBx4Pe1dhx7orNGo6WJ8Mjv1V2bw",
+            timestamp: "June 06, 2025 23:29:11 +UTC"
+        },
+        {
+            walletName: "pow",
+            from: "GRtUWR9VuXgTxaV3GL5cgdqEs799EhQzxpg6RUUSLLUK",
+            to: "8zFZHuSRuDpuAR7J6FzwyF3vKNx4CVW3DFHJerQhc7Zd",
+            amount: 671.497,
+            signature: "2ZnpiL7pJsdqnkP7vh4N2uidnj3Zt7jp14MA8nnzuLQcdD8jznSDTrDd8kc2VbuvDjidS2RRFLJpzkDrfk7S1nf9",
+            timestamp: "June 06, 2025 23:03:59 +UTC"
+        },
+        {
+            walletName: "gorilla",
+            from: "HK2TKdCZM8j3dAhR4wUrsa28MVKn6vgLPMi7QhsBHgXM",
+            to: "DpNVrtA3ERfKzX4F8Pi2CVykdJJjoNxyY5QgoytAwD26",
+            amount: 142.68,
+            signature: "4Z1PRwdX4NSa9BdGsr2z9Xf2FNcYhGyNHXC5U99UDuHBhnJ69siXUxiavHjnZV6GAq5dm5vXdJRWh96EThyenhvC",
+            timestamp: "June 06, 2025 22:08:59 +UTC"
+        },
+        {
+            walletName: "lexapro",
+            from: "9YYGgphrSopDBaQTAxpuqwZDLgdRjJWhYQCmAFrfqrXq",
+            to: "8yJFWmVTQq69p6VJxGwpzW7ii7c5J9GRAtHCNMMQPydj",
+            amount: 100.0,
+            signature: "5JJw3k4kb6PUjVc5tGEs7ygPbPYK7k2STU69cMEkroRNZBK48z4xots4TXht6FZDbckZEmsprqxpc5Fxxp9gnPvp",
+            timestamp: "June 05, 2025 05:21:28 +UTC"
+        },
+        {
+            walletName: "meechie",
+            from: "6qkh2JcHt3ctFeiL4HBn1e9NU5aPw25XNhtgEv6ZCJ4U",
+            to: "831qmkeGhfL8YpcXuhrug6nHj1YdK3aXMDQUCo85Auh1",
+            amount: 101.991,
+            signature: "4U9sKJYpLfBNz93KvJP5yAEPtGsHAPp37rgGjTbUEqKWKnBBG8Lbxi2daQiT8q7WJXxyyVLtP6AM5quvA5gTyNSk",
+            timestamp: "May 13, 2025 13:54:09 +UTC"
+        },
+        {
+            walletName: "kaden",
+            from: "3pZ59YENxDAcjaKa3sahZJBcgER4rGYi4v6BpPurmsGj",
+            to: "BNismNsqqCKLcMzJeM2x2nMx53VMKytvkNKubJ1Tx9QC",
+            amount: 161.13,
+            signature: "2TUw5XKHX3YoK5tsJDe5j8LK7U2Fu2eZB1yiWxuHDrBfxexF5m2DFR76RnnX3JKonNWTndszgNboEtTeuQ93ahdu",
+            timestamp: "May 05, 2025 05:23:07 +UTC"
+        },
+        {
+            walletName: "Jalen",
+            from: "F72vY99ihQsYwqEDCfz7igKXA5me6vN2zqVsVUTpw6qL",
+            to: "CW9qWGDXQqDAKeRyPbBZ352cKgNqEUtpfKrR7n9PqZa5",
+            amount: 494.71,
+            signature: "5cDBEC5mKctZ7JpqRjbgtZDG2JBqND1SAFUx5gzh4dqtEL9gth1CaBqqb3FHjG5EGv6KoxCxztvZWp32x3vehJqF",
+            timestamp: "March 05, 2025 20:55:51 +UTC"
+        }
+    ]
+};
+
 
 // Export for use in main script
 window.GHOST_DATA = GHOST_DATA;
